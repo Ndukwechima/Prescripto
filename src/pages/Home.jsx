1d46ea8c182167bfdadc627f7bf59b5449c5abd6
chimae <chimandukwe.r@gmail.com>
2025-01-31 09:12:25 +0000

    Completed TopDoctorAppointment sec with Context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import SpecialityMenu from '../components/SpecialityMenu'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <SpecialityMenu />
+      <TopDoctors />
     </div>
   )
 }
