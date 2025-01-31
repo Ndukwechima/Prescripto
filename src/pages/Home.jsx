3b1423d9147488931fbcf02e77cec290acc4a455
chimae <chimandukwe.r@gmail.com>
2025-01-31 06:37:31 +0000

    Completed the Speciality of the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import SpecialityMenu from '../components/SpecialityMenu'
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <SpecialityMenu />
     </div>
   )
 }
